5a1a8a36e6533a9562491c59b78c7ca2c14bb2be
Patrick Dolan <dolanp1992@gmail.com>
2024-02-15 13:57:46 -0800

    remove unneeded files and set up basic structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </>
+  );
+}
+
+export default App;
