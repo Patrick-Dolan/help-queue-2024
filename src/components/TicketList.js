6dd7e02d8c363156f07146bda209c43652d7ac30
Patrick Dolan <dolanp1992@gmail.com>
2024-02-19 10:17:47 -0800

    add create and read functionality with state

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,28 +1,11 @@
 import Ticket from './Ticket';
+import PropTypes from 'prop-types';
 
-function TicketList() {
-  const mainTicketList = [
-    {
-      names: 'Thato and Haley',
-      location: '3A',
-      issue: 'Firebase won\'t save record. Halp.'
-    },
-    {
-      names: 'Sleater and Kinney',
-      location: '4B',
-      issue: 'Prop types are throwing an error.'
-    },
-    {
-      names: 'Imani & Jacob',
-      location: '9F',
-      issue: 'Child component isn\'t rendering.'
-    }
-  ];
-
+function TicketList(props) {
   return (
     <>
       <hr />
-      {mainTicketList.map((ticket, index) =>
+      {props.ticketList.map((ticket, index) =>
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
@@ -31,5 +14,9 @@ function TicketList() {
     </>
   );
 }
+
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketList;
