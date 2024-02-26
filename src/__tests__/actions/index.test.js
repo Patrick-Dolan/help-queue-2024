b9f3f92fdebe1471ec998a4fd32b82e44e1abc22
Patrick Dolan <dolanp1992@gmail.com>
2024-02-26 14:35:33 -0800

    refactor Redux to use actions constants

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,17 +1,18 @@
 import * as actions from '../../actions';
+import * as constants from '../../actions/ActionTypes';
 
 describe("actions", () => {
 
   test("deleteTicket should create DELETE_TICKET action", () => {
     expect(actions.deleteTicket(1)).toEqual({
-      type: "DELETE_TICKET",
+      type: constants.DELETE_TICKET,
       id: 1
     });
   });
 
   test("toggleForm should create TOGGLE_FORM action", () => {
     expect(actions.toggleForm()).toEqual({
-      type: "TOGGLE_FORM"
+      type: constants.TOGGLE_FORM
     });
   });
 
@@ -22,7 +23,7 @@ describe("actions", () => {
       issue: "Redux not working!", 
       id: 1
     })).toEqual({
-      type: "ADD_TICKET",
+      type: constants.ADD_TICKET,
       names: "Jo and Jasmine",
       location: "3E",
       issue: "Redux not working!",
